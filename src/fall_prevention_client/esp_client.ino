fbfcd75290fa81c4a7a7eb4b83b5b7be4f978bd3
EdenLevi <47477081+EdenLevi@users.noreply.github.com>
2023-01-22 22:31:49 +0200

    Reconnection to WiFi and server

--- src/fall_prevention_client/esp_client.ino
+++ src/fall_prevention_client/esp_client.ino
@@ -1,9 +1,7 @@
 #include <Adafruit_ADS1X15.h>
-#include <HX711_ADC.h>
-#include <EEPROM.h>
 #include <Wire.h>
 
-#include "fall_prevention_client.h"
+#include "src/fall_prevention_client/fall_prevention_client.h"
 
 /* Consts. */
 #define WEIGHT_SDA      4
@@ -11,12 +9,14 @@
 #define FSR_CAL_FACTOR  19.5
 #define WCAL_FACTOR     1
 #define TX_RATE         9600
-#define SAMPLE_DELAY    200
+#define SAMPLE_DELAY    300
 #define FS_RES          5.0
 #define FS_DIV          1023.0
 #define VAL_RES         3
-#define WIFI_SSID       "Fall Prevention Wifi"
-#define WIFI_PASS       "95334448"
+#define WIFI_SSID       "Fall Prevention Wifi" // router
+#define WIFI_PASS       "95334448" // router
+// #define WIFI_SSID       "Pi wifi" // hotspot
+// #define WIFI_PASS       "31415926" // hotspot
 #define SERVER_PORT     13380
 #define WS_SCL          22
 #define ADS_ADDR        0x48
@@ -38,8 +38,6 @@ enum SENSOR {
     RFSR2,
     RFSR3,
     RFSR4,
-    /* Weight sensor */
-    WEIGHT_SENSOR,
     NUM_SENSORS
 };
 
@@ -53,19 +51,17 @@ static const int sensor_gpio_map[] {
     [RFSR2] = 1,
     [RFSR3] = 2,
     [RFSR4] = 3,
-    [WEIGHT_SENSOR] = 33,
 };
 
 
 /* Globals */
 WiFiClient client;
-IPAddress server(192, 168, 0, 100);
+IPAddress server(192, 168, 0, 101); // router
+// IPAddress server(192, 168, 6, 232); // hotspot
 const int port = SERVER_PORT;
 char main_buffer[MSG_BUFFER];
 
 Adafruit_ADS1015 ads;
-
-HX711_ADC weight_sensor(WEIGHT_SDA, WEIGHT_SCL);
 
 
 void setup()
@@ -75,11 +71,6 @@ void setup()
     /* Init Flex. Force sensors*/
     for (int sensor = 0; sensor < NUM_FFSR; sensor++)
         pinMode(sensor_gpio_map[sensor], INPUT);
-
-    /* Init Weight sensor */
-    //weight_sensor.begin();
-    //weight_sensor.start(2000, false);
-    //weight_sensor.setCalFactor(WCAL_FACTOR);
 
     /* Init I2C ADS */
     ads.begin(ADS_ADDR);
@@ -95,6 +86,16 @@ void setup()
 void loop()
 {
     int sensor_data, idx;
+    
+    /* if not currently connected to WiFi (connection dropped) */
+    if (WiFi.status() != WL_CONNECTED) {
+        wifi_connect(WIFI_SSID, WIFI_PASS);
+        server_connect(&client, server, port);
+    }
+    /* if not currently connected to server (connection dropped) */
+    else if (!client.connected()) {
+        server_connect(&client, server, port);
+    }
 
     idx = sprintf(main_buffer, "Sensors reads: ");
 
@@ -105,16 +106,10 @@ void loop()
     }
 
     /* Get RFSR data */
-    for (int sensor = NUM_FFSR; sensor < NUM_SENSORS - 1; sensor++) {
+    for (int sensor = NUM_FFSR; sensor < NUM_SENSORS; sensor++) {
         sensor_data = ads.readADC_SingleEnded(sensor_gpio_map[sensor]);
         idx += sprintf(main_buffer + idx, "%d ", sensor_data);
     }
-
-    /* Get Weight data */
-    //while (weight_sensor.update()) {}
-    //sensor_data = weight_sensor.getData();
-    //idx += sprintf(main_buffer + idx, "%d ", sensor_data);
-    idx += sprintf(main_buffer + idx, "%d ", 31415926);
 
     Serial.println(main_buffer); 
     client.println(main_buffer); 
